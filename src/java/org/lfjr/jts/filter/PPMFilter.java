74c6665ff41402f64ea33eb18e8c90a7fa6c53f9
Emmanuel Bourg <smanux@lfjr.net>
2002-09-27 16:56:59 +0000

    ppm filter update

--- src/java/org/lfjr/jts/filter/PPMFilter.java
+++ src/java/org/lfjr/jts/filter/PPMFilter.java
@@ -26,76 +26,92 @@ import org.lfjr.jts.config.*;
 /**
  * A filter computing and displaying the number of pieces dropped per minute
  * by each player.
- * 
+ *
  * @author Emmanuel Bourg
  * @version $Revision$, $Date$
  */
 public class PPMFilter extends GenericFilter
 {
     private long totalTime;
-    private long lastStop;
+    private long lastStart;
     private int blockCount[];
     private long slotTime[];
 
-    
+
     public void init(FilterConfig conf)
     {
         blockCount = new int[6];
         slotTime = new long[6];
     }
-    
+
     public void onStartGame(Message m, List out)
     {
+        // reseting the played time for this game
         totalTime = 0;
-        lastStop = new Date().getTime();
+
+        // registering the date of the last start/resume
+        lastStart = new Date().getTime();
+
+        // clearing block count and played time for each player
         Arrays.fill(blockCount, 0);
         Arrays.fill(slotTime, 0);
-        
+
         out.add(m);
     }
-    
+
     public void onEndGame(Message m, List out)
     {
         // updating play time for each slot
         long now = new Date().getTime();
-        for (int slot=0; slot<6; slot++) { slotTime[slot] = slotTime[slot] + (now - lastStop); }
-        
+        for (int slot=0; slot<6; slot++) { slotTime[slot] = slotTime[slot] + (now - lastStart); }
+
         // displaying results
-        // ...
-        
+        for (int slot=0; slot<6; slot++)
+        {
+            TetriNETClient client = getChannel().getPlayer(slot);
+            TetriNETPlayer player = client.getPlayer();
+            
+            System.out.println(player.getName() + " : " + "time played="+slotTime[slot]+" ");             
+                   	
+        }
+
         out.add(m);
     }
-    
+
     public void onPause(Message m, List out)
     {
         // updating global play time
         long now = new Date().getTime();
-        totalTime = totalTime + (now - lastStop);
-        
-        out.add(m); 
+        totalTime = totalTime + (now - lastStart);
+
+        out.add(m);
     }
 
 
     public void onResume(Message m, List out)
     {
-        lastStop = new Date().getTime();
-        
-        out.add(m); 
-    }    
+        lastStart = new Date().getTime();
+
+        out.add(m);
+    }
 
     public void onField(Message m, List out)
     {
         // increasing block count for the updated slot
         int slot = m.getIntParameter(0);
-        blockCount[slot]++;        
-        
-        out.add(m); 
+        blockCount[slot]++;
+
+        out.add(m);
     }
-    
+
 
     public String getName() { return "PPM Filter"; }
 
-    public String getDescription() { return "Counts pieces per minute"; }
+    public String getDescription() 
+    { 
+        return "Counts pieces dropped per minute by each player in a channel "
+               + "and displays the result at the end of the game."; 
+    }
 
     public String getVersion() { return "1.0"; }
 
