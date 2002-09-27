ba8800d417c7441e507ac33b4b0b1bbe1cff54f1
Emmanuel Bourg <smanux@lfjr.net>
2002-09-27 16:49:04 +0000

    implemented execute(), getCommands() and clear() made getCommand() case insensitive

--- src/java/org/lfjr/jts/commands/CommandManager.java
+++ src/java/org/lfjr/jts/commands/CommandManager.java
@@ -20,8 +20,8 @@
 package org.lfjr.jts.commands;
 
 import java.util.*;
-
 import org.lfjr.jts.*;
+import org.apache.commons.collections.*;
 
 /**
  * CommandManager
@@ -33,10 +33,12 @@ public class CommandManager
 {
     private static CommandManager instance = new CommandManager();
     private Map commands;
+    private Map commandSet;
 
     private CommandManager()
     {
         commands = new TreeMap();
+        commandSet = new TreeMap();
     }
 
     public static CommandManager getInstance()
@@ -51,43 +53,104 @@ public class CommandManager
         {
             addCommandAlias(command, aliases[i]);
         }
+
+        if (aliases.length > 0) commandSet.put(command.getAliases()[0], command);
     }
 
     public void addCommandAlias(Command command, String alias)
     {
-        commands.put(alias, command);
+        commands.put(alias.toLowerCase(), command);
     }
 
+    /**
+     * Return the command matching the specified alias. The alias is not
+     * case sensitive. If no command matches the exact alias specified, 
+     * it will return the first command starting with the alias.
+     */
     public Command getCommand(String alias)
     {
-        Command command = (Command)commands.get(alias);
+        Command command = (Command)commands.get(alias.toLowerCase());
 
         if (command == null)
         {
             Iterator aliases = commands.keySet().iterator();
             while (command == null && aliases.hasNext())
             {
-                String name = (String)aliases.next();
-                if (name.startsWith(alias)) command = (Command)commands.get(name);
+                String name = ((String)aliases.next()).toLowerCase();
+                if (name.startsWith(alias.toLowerCase()))
+                {
+                    command = (Command)commands.get(name);
+                }
             }
         }
 
         return command;
     }
 
+    /**
+     * Return all commands available to clients with the lowest
+     * access level.
+     */
     public Iterator getCommands()
     {
          return getCommands(0);
     }
 
-    public Iterator getCommands(int accesLevel)
+    /**
+     * Return all commands available to clients with the specified
+     * access level.
+     */
+    public Iterator getCommands(final int accessLevel)
     {
-        return commands.values().iterator();
+        Predicate availableToLevel = new Predicate() {
+            public boolean evaluate(Object obj)
+            {
+                Command command = (Command)obj;
+                return command.getAccessLevel() <= accessLevel;
+            }
+        };
+
+        return new FilterIterator(commandSet.values().iterator(), availableToLevel);
     }
 
+    /**
+     * Execute the command specified in the message. This method checks if
+     * the client has the access level required to use the command.
+     */
     public void execute(Message m)
     {
+        Command command = getCommand(m.getStringParameter(1).substring(1));
+        TetriNETClient client = (TetriNETClient)m.getSource();
+        if (command == null)
+        {
+            Message response = new Message(Message.MSG_PLINE);
+            response.setParameters(new Object[] { new Integer(0), ChatColors.red + "Invalid /command!" });
+            client.sendMessage(response);
+        }
+        else
+        {
+            // check the access level
+            if (client.getPlayer().getAccessLevel() >= command.getAccessLevel())
+            {
+                command.execute(m);
+            }
+            else
+            {
+                // denying access
+                Message response = new Message(Message.MSG_PLINE);
+                response.setParameters(new Object[] { new Integer(0), ChatColors.red + "You don't have access to this command." });
+                client.sendMessage(response);
+            }
+        }
+    }
 
+    /**
+     * Clear all commands.
+     */
+    public void clear()
+    {
+        commands.clear();
+        commandSet.clear();
     }
 
 }
