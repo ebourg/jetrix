76e283cc1d1d600f76f9bcbb7d0770fd03237eec
Emmanuel Bourg <smanux@lfjr.net>
2002-11-29 18:38:34 +0000

    Replaced the ServerConsole with a ConsoleClient

---
+++ src/java/net/jetrix/clients/ConsoleClient.java
@@ -0,0 +1,109 @@
+/**
+ * Jetrix TetriNET Server
+ * Copyright (C) 2001-2002  Emmanuel Bourg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package net.jetrix;
+
+import java.io.*;
+import java.net.*;
+import net.jetrix.config.*;
+
+/**
+ * Command line console.
+ *
+ * @author Emmanuel Bourg
+ * @version $Revision$, $Date$
+ */
+public class ConsoleClient implements Client
+{
+    private BufferedReader dis = new BufferedReader(new InputStreamReader(System.in));
+    private ServerConfig conf;
+    private Protocol protocol;
+
+    public ConsoleClient()
+    {
+        conf = Server.getInstance().getConfig();
+        protocol = ProtocolManager.getInstance().getProtocol("net.jetrix.protocols.ConsoleProtocol");
+    }
+
+    public Protocol getProtocol()
+    {
+        return this.protocol;
+    }
+
+    public void run()
+    {
+        while (conf.isRunning())
+        {
+            try
+            {
+                Message m = receiveMessage();
+                if (m == null) continue;
+                Server.getInstance().sendMessage(m);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessage(Message message)
+    {
+        System.out.println(protocol.translate(message));
+    }
+
+    public Message receiveMessage() throws IOException
+    {
+        String cmd = dis.readLine();
+        Message m = protocol.getMessage(cmd);
+        m.setSource(this);
+        
+        return m;
+    }
+
+    public InetAddress getInetAddress()
+    {
+        return conf.getHost();
+    }
+
+    public void setChannel(Channel channel) { }
+
+    public Channel getChannel()
+    {
+        return null;
+    }
+
+    public User getUser()
+    {
+        return null;
+    }
+
+    public String getVersion()
+    {
+        return "1.0";
+    }
+
+    public String getType()
+    {
+        return "Console";
+    }
+
+    public void disconnect() { }
+
+}
---
+++ src/java/net/jetrix/protocols/ConsoleProtocol.java
@@ -0,0 +1,97 @@
+/**
+ * Jetrix TetriNET Server
+ * Copyright (C) 2001-2002  Emmanuel Bourg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package net.jetrix.protocols;
+
+import java.util.*;
+import net.jetrix.*;
+import net.jetrix.config.*;
+import net.jetrix.messages.*;
+
+/**
+ * Protocol to communicate with a console client
+ *
+ * @author Emmanuel Bourg
+ * @version $Revision$, $Date$
+ */
+public class ConsoleProtocol extends Protocol
+{
+
+    /**
+     * Return the name of this protocol
+     */
+    public String getName()
+    {
+        return "console";
+    }
+
+    /**
+     * Parse the specified string and return the corresponding server
+     * message for this protocol.
+     */
+    public Message getMessage(String line)
+    {
+        StringTokenizer st = new StringTokenizer(line);
+        Message m = null;
+
+        if (st.hasMoreTokens())
+        {
+            String firstWord = st.nextToken();
+
+            CommandMessage command = new CommandMessage();
+            command.setCommand(firstWord.substring(1));
+            command.setText(line.substring(line.indexOf(" ") + 1));
+            while (st.hasMoreTokens()) { command.addParameter(st.nextToken()); }
+            m = command;
+        }
+
+        return m;
+    }
+
+    /**
+     * Translate the specified message into a string that will be sent
+     * to a client using this protocol.
+     */
+    public String translate(Message m)
+    {
+        if ( m instanceof TextMessage) return translate((TextMessage)m);
+        else
+        {
+            return null;
+        }
+    }
+
+    public String translate(TextMessage m)
+    {
+        StringBuffer message = new StringBuffer();
+        message.append(applyStyle(m.getText(getLocale())));
+        return message.toString();
+    }
+
+    public Map getStyles()
+    {
+        return null;
+    }
+
+    public String toString()
+    {
+        return "[Protocol name=" + getName() + "]";
+    }
+
+}
