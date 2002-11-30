a321aa7f3483f4ee2dddf4934810fc10790a4026
Emmanuel Bourg <smanux@lfjr.net>
2002-11-30 18:44:12 +0000

    code formatting

--- src/java/net/jetrix/commands/VersionCommand.java
+++ src/java/net/jetrix/commands/VersionCommand.java
@@ -59,8 +59,12 @@ public class VersionCommand implements Command
         Client client = (Client)m.getSource();
 
         String version1 = "<darkBlue><b>JetriX/" + ServerConfig.VERSION + " (build:@build.time@)";
-        String version2 = "<purple>VM<darkBlue>: " + System.getProperty("java.vm.name") + " " + System.getProperty("java.vm.version") + " " + System.getProperty("java.vm.info");
-        String version3 = "<purple>OS<darkBlue>:" + System.getProperty("os.name") + " " + System.getProperty("os.version") +"; " + System.getProperty("os.arch");
+        String version2 = "<purple>VM<darkBlue>: " + System.getProperty("java.vm.name") 
+                          + " " + System.getProperty("java.vm.version") 
+                          + " " + System.getProperty("java.vm.info");
+        String version3 = "<purple>OS<darkBlue>: " + System.getProperty("os.name") 
+                          + " " + System.getProperty("os.version") 
+                          + "; " + System.getProperty("os.arch");
 
         client.sendMessage(new PlineMessage(version1));
         client.sendMessage(new PlineMessage(version2));
