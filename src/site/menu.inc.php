4e2e44145c20df74c29977e228268def509e6c6f
Emmanuel Bourg <smanux@lfjr.net>
2003-01-19 23:57:24 +0000

    Added link to CVS commits

--- src/site/menu.inc.php
+++ src/site/menu.inc.php
@@ -8,13 +8,13 @@
           <li><a href="soon.php">Roadmap</a></li>
           <li><a href="license.php">License</a></li>
 	</ul>
-            
+
         <h1>Download</h1>
         <ul>
           <li><a href="https://sourceforge.net/project/showfiles.php?group_id=52188">Binaries</a></li>
           <li><a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/jetrix/">Source Code</a></li>
         </ul>
-                
+
         <h1>Documentation</h1>
         <ul>
           <li><a href="architecture.php">Architecture</a></li>
@@ -24,14 +24,15 @@
           <li><a href="soon.php">Login procedure</a></li>
           <li><a href="query-protocol.php">Query protocol</a></li>
           <li><a href="soon.php">Colors</a></li>
-        </ul>   
-        
+        </ul>
+
         <h1>Project</h1>
         <ul>
           <li><a href="http://sourceforge.net/cvs/?group_id=52188">CVS repository</a></li>
           <li><a href="https://sourceforge.net/forum/forum.php?forum_id=172941">Forum</a></li>
           <li><a href="https://sourceforge.net/tracker/?group_id=52188&atid=466002">Bugs</a></li>
           <li><a href="changelog.php">Changelog</a></li>
-        </ul>        
+          <li><a href="https://sourceforge.net/mailarchive/forum.php?forum_id=12998">CVS commits</a></li>
+        </ul>
       </div>
       
