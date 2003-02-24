a7e47f747018daba06a70331dbfa7b2358336dc3
Emmanuel Bourg <smanux@lfjr.net>
2003-02-24 13:54:03 +0000

    Fix for the ConcurrentModificationException when the client list is modified and read concurrently

--- src/java/net/jetrix/ClientRepository.java
+++ src/java/net/jetrix/ClientRepository.java
@@ -1,6 +1,6 @@
 /**
  * Jetrix TetriNET Server
- * Copyright (C) 2001-2002  Emmanuel Bourg
+ * Copyright (C) 2001-2003  Emmanuel Bourg
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -52,7 +52,9 @@ public class ClientRepository
 
     private ClientRepository()
     {
-        clients = new TreeMap();
+        FastTreeMap map = new FastTreeMap();
+        map.setFast(true);
+        clients = map;
     }
 
     public static ClientRepository getInstance()
