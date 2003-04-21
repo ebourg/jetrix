7653dafeea557d0ba717f86c2fdd6a735375f551
Emmanuel Bourg <smanux@lfjr.net>
2003-04-21 01:53:02 +0000

    Added W3C logo

--- src/site/footer.inc.php
+++ src/site/footer.inc.php
@@ -8,6 +8,12 @@
       <div class="hr"></div>
     </td>
   </tr>
+  <tr>
+    <td colspan="2">
+      <a href="http://validator.w3.org/check/referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0!" height="31" width="88" /></a>
+      <a href="http://jigsaw.w3.org/css-validator/"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a>
+    </td>
+  </tr>
 </tbody>
 </table>
 
