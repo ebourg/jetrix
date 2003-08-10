2b66a9a4e95d72e8d9a44984e4a0cd95c0c1eb44
Emmanuel Bourg <smanux@lfjr.net>
2003-08-10 23:31:52 +0000

    post 0.1.2 update

--- src/site/roadmap.php
+++ src/site/roadmap.php
@@ -6,27 +6,17 @@ This page summarizes the developpement plan for the next releases. The first
 goal is to match the features of tetrinetx and then add the cool features up 
 to the version 1.0.
 
-<h2>JetriX 0.1.2</h2>
-
-<ul>
-  <li>Basic TSpec support</li>
-  <li>Basic web administration (read only)</li>
-  <li>Winlists</li>
-  <li><tt>/move</tt> command</li>
-</ul>
 
 <h2>JetriX 0.1.3</h2>
 
 <ul>
   <li>Full TSpec support</li>
   <li>Ban list</li>
-  <li>PPM</li>
 </ul>
 
 <h2>JetriX 0.1.4</h2>
 
 <ul>
-  <li>Query protocol support</li>
   <li>IRC Clients support</li>  
 </ul>
 
