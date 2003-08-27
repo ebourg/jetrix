c6b7d11b4f82a092dbf1792b062eec507f28f8e3
Emmanuel Bourg <smanux@lfjr.net>
2003-08-27 02:23:31 +0000

    Added SF news on the website

--- src/site/index.php
+++ src/site/index.php
@@ -2,16 +2,19 @@
       
 <h1>What is JetriX ?</h1>
 
-JetriX is a new generation TetriNET server written in Java and designed for maximum scalability and extensibility. It features a pluggable filter and command API allowing easy channel behavior customization that requires little knowledge of the server's inner functioning. The ambitious goal of this project is to create the ideal plateform for server side TetriNET programmers. JetriX is open source and developped under the GPL.
+JetriX is a new generation TetriNET server written in Java and designed for 
+maximum scalability and extensibility. It features a pluggable filter and 
+command API allowing easy channel behavior customization that requires little 
+knowledge of the server's inner functioning. The ambitious goal of this project
+is to create the ideal plateform for server side TetriNET programmers. JetriX 
+is open source and developped under the <a href="license.php">GNU General 
+Public License</a>.
 <br /><br />
 
 Check out the <a href="features.php">features</a> !
 
 <h1>News</h1>
 
-<div class="news">
-
-
-</div>
+<? include("news.html") ?>
 
 <? include("footer.inc.php") ?>
