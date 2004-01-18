6854aa7dbfd62d5a52809db3debe3e8fd0928d41
Emmanuel Bourg <smanux@lfjr.net>
2004-01-18 17:36:17 +0000

    LogoFilter displaying a logo on the field at the end of the game.

---
+++ src/java/net/jetrix/filter/LogoFilter.java
@@ -0,0 +1,74 @@
+/**
+ * Jetrix TetriNET Server
+ * Copyright (C) 2001-2003  Emmanuel Bourg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package net.jetrix.filter;
+
+import java.util.*;
+
+import net.jetrix.messages.*;
+
+/**
+ * Display a logo on the field of players loosing the game.
+ *
+ * @author Emmanuel Bourg
+ * @version $Revision$, $Date$
+ */
+public class LogoFilter extends GenericFilter
+{
+
+    private short jetrixLogo[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                  0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+                                  0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0};
+
+    public void onMessage(PlayerLostMessage m, List out)
+    {
+        // send closing screen
+        StringBuffer screenLayout = new StringBuffer();
+        for (int i = 0; i < 12 * 22; i++)
+        {
+            screenLayout.append(((int) (Math.random() * 5 + 1)) * (1 - jetrixLogo[i]));
+        }
+        FieldMessage endingScreen = new FieldMessage();
+        endingScreen.setSlot(m.getSlot());
+        endingScreen.setField(screenLayout.toString());
+
+        out.add(m);
+        out.add(endingScreen);
+    }
+
+}
