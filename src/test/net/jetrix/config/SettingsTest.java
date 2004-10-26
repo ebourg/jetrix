4b206329583410d945ca6db0e1bc565430d90b8e
Emmanuel Bourg <smanux@lfjr.net>
2004-10-26 16:15:10 +0000

    full coverage

--- src/test/net/jetrix/config/SettingsTest.java
+++ src/test/net/jetrix/config/SettingsTest.java
@@ -29,6 +29,37 @@ import junit.framework.*;
  */
 public class SettingsTest extends TestCase
 {
+    private Settings defaultSettings;
+
+    protected void setUp() throws Exception
+    {
+        defaultSettings = new Settings(false);
+        defaultSettings.setAverageLevels(true);
+        defaultSettings.setClassicRules(true);
+        defaultSettings.setLevelIncrease(2);
+        defaultSettings.setLinesPerLevel(4);
+        defaultSettings.setLinesPerSpecial(6);
+        defaultSettings.setSameBlocks(true);
+        defaultSettings.setSpecialAdded(8);
+        defaultSettings.setSpecialCapacity(10);
+        defaultSettings.setStackHeight(12);
+        defaultSettings.setStartingLevel(14);
+        defaultSettings.setSuddenDeathDelay(16);
+        defaultSettings.setSuddenDeathLinesAdded(18);
+        defaultSettings.setSuddenDeathMessage("Hurry Up!");
+        defaultSettings.setSuddenDeathTime(20);
+
+        defaultSettings.setOccurancy(Block.LINE, 10);
+        defaultSettings.setOccurancy(Special.GRAVITY, 10);
+
+        Settings.setDefaultSettings(defaultSettings);
+    }
+
+    public void testGetDefaultSettings()
+    {
+        assertEquals(defaultSettings, Settings.getDefaultSettings());
+    }
+
     public static void testNormalize1()
     {
         Settings s = new Settings();
@@ -70,6 +101,14 @@ public class SettingsTest extends TestCase
         assertTrue("Erreur de normalisation", equals(tab4, tab5));
     }
 
+    public static void testNormalize6()
+    {
+        Settings s = new Settings();
+        int[] tab = { 0, -10 };
+        s.normalize(tab);
+        assertEquals("Erreur de normalisation", 0, sum(tab));
+    }
+
     private static long sum(int[] tab)
     {
         long s = 0;
@@ -77,6 +116,9 @@ public class SettingsTest extends TestCase
         return s;
     }
 
+    /**
+     * todo: replace with ArrayAssert from junit-addons
+     */
     private static boolean equals(int[] a, int[] b)
     {
         boolean equals = true;
@@ -96,6 +138,134 @@ public class SettingsTest extends TestCase
         }
 
         return equals;
+    }
+
+    public void testAverageLevels()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getAverageLevels(), settings.getAverageLevels());
+        settings.setAverageLevels(false);
+        assertEquals(false, settings.getAverageLevels());
+    }
+
+    public void testClassicRules()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getClassicRules(), settings.getClassicRules());
+        settings.setClassicRules(false);
+        assertEquals(false, settings.getClassicRules());
+    }
+
+    public void testLevelIncrease()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getLevelIncrease(), settings.getLevelIncrease());
+        settings.setLevelIncrease(0);
+        assertEquals(0, settings.getLevelIncrease());
+    }
+
+    public void testLinesPerLevel()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getLinesPerLevel(), settings.getLinesPerLevel());
+        settings.setLinesPerLevel(0);
+        assertEquals(0, settings.getLinesPerLevel());
+    }
+
+    public void testLinesPerSpecial()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getLinesPerSpecial(), settings.getLinesPerSpecial());
+        settings.setLinesPerSpecial(0);
+        assertEquals(0, settings.getLinesPerSpecial());
+    }
+
+    public void testSameBlocks()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getSameBlocks(), settings.getSameBlocks());
+        settings.setSameBlocks(false);
+        assertEquals(false, settings.getSameBlocks());
+    }
+
+    public void testSpecialAdded()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getSpecialAdded(), settings.getSpecialAdded());
+        settings.setSpecialAdded(0);
+        assertEquals(0, settings.getSpecialAdded());
+    }
+
+    public void testSpecialCapacity()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getSpecialCapacity(), settings.getSpecialCapacity());
+        settings.setSpecialCapacity(0);
+        assertEquals(0, settings.getSpecialCapacity());
+    }
+
+    public void testStartingLevel()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getStartingLevel(), settings.getStartingLevel());
+        settings.setStartingLevel(0);
+        assertEquals(0, settings.getStartingLevel());
+    }
+
+    public void testStackHeight()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getStackHeight(), settings.getStackHeight());
+        settings.setStackHeight(0);
+        assertEquals(0, settings.getStackHeight());
+    }
+
+    public void testSuddenDeathDelay()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getSuddenDeathDelay(), settings.getSuddenDeathDelay());
+        settings.setSuddenDeathDelay(0);
+        assertEquals(0, settings.getSuddenDeathDelay());
+    }
+
+    public void testSuddenDeathLinesAdded()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getSuddenDeathLinesAdded(), settings.getSuddenDeathLinesAdded());
+        settings.setSuddenDeathLinesAdded(0);
+        assertEquals(0, settings.getSuddenDeathLinesAdded());
+    }
+
+    public void testSuddenDeathMessage()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getSuddenDeathMessage(), settings.getSuddenDeathMessage());
+        settings.setSuddenDeathMessage("");
+        assertEquals("", settings.getSuddenDeathMessage());
+    }
+
+    public void testSuddenDeathTime()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getSuddenDeathTime(), settings.getSuddenDeathTime());
+        settings.setSuddenDeathTime(0);
+        assertEquals(0, settings.getSuddenDeathTime());
+    }
+
+    public void testBlockOccurancy()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getOccurancy(Block.LINE), settings.getOccurancy(Block.LINE));
+        settings.setOccurancy(Block.LINE, 50);
+        assertEquals(50, settings.getOccurancy(Block.LINE));
+    }
+
+    public void testSpecialOccurancy()
+    {
+        Settings settings = new Settings();
+        assertEquals(defaultSettings.getOccurancy(Special.GRAVITY), settings.getOccurancy(Special.GRAVITY));
+        settings.setOccurancy(Special.GRAVITY, 50);
+        assertEquals(50, settings.getOccurancy(Special.GRAVITY));
     }
 
 }
