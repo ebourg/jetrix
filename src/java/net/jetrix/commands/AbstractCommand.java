4bf5c01fc4e4e158f9cb648d918f3738b9e3db2f
Emmanuel Bourg <smanux@lfjr.net>
2004-12-06 14:07:54 +0000

    Commands can now be hidden from the /help list It's now possible to override the default access level of a command

---
+++ src/java/net/jetrix/commands/AbstractCommand.java
@@ -0,0 +1,59 @@
+/**
+ * Jetrix TetriNET Server
+ * Copyright (C) 2004  Emmanuel Bourg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package net.jetrix.commands;
+
+import java.util.logging.Logger;
+
+import net.jetrix.AccessLevel;
+
+/**
+ * Abstract command.
+ *
+ * @author Emmanuel Bourg
+ * @version $Revision$, $Date$
+ * @since 0.1.4
+ */
+public abstract class AbstractCommand implements Command
+{
+    protected int level = AccessLevel.PLAYER;
+    protected boolean hidden;
+
+    protected static Logger log = Logger.getLogger("net.jetrix");
+
+    public int getAccessLevel()
+    {
+        return level;
+    }
+
+    public void setAccessLevel(int level)
+    {
+        this.level = level;
+    }
+
+    public boolean isHidden()
+    {
+        return hidden;
+    }
+
+    public void setHidden(boolean hidden)
+    {
+        this.hidden = hidden;
+    }
+}
