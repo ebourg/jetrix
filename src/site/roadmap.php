3c58d927566776a555ff6e57edc48cc8f8b9f434
Emmanuel Bourg <smanux@lfjr.net>
2004-12-16 01:06:09 +0000

    update, road map to Jetrix 1.0

--- src/site/roadmap.php
+++ src/site/roadmap.php
@@ -7,30 +7,38 @@ goal is to match the features of tetrinetx and then add the cool features up
 to the version 1.0.
 
 
-
-<h2>JetriX 0.1.4</h2>
-
-<ul>
-  <li>IRC Clients support</li>
-  <li>Services</li>
-</ul>
-
-<h2>JetriX 0.1.5</h2>
-
-<ul>
-  <li>Dynamic channel creation</li>
-  <li>Full web administration</li>
-  <li>Sudden death</li>
-</ul>
-
-<h2>JetriX 0.2</h2>
-
+<h2>Jetrix 0.2</h2>
 <ul>
   <li>Feature equivalent to tetrinetx</li>
-  <li>tetrinetx to Jetrix migration tool</li>
-  <li>Database integration</li>
-  <li>Player authentication</li>
+  <li>Services</li>
+  <li>Sudden death</li>
+  <li>Dynamic channel creation</li>
 </ul>
 
+<h2>Jetrix 0.3</h2>
+
+<ul>
+  <li>Database integration</li>
+  <li>Player authentication</li>
+  <li>Full support of IRC clients</li>
+  <li>Full web administration</li>
+  <li>Friends list</li>
+</ul>
+
+<h2>Jetrix 0.4</h2>
+
+<ul>
+  <li>Player stats</li>
+  <li>Puzzle channels</li>
+  <li>tetrinetx to Jetrix migration tool</li>
+</ul>
+
+<h2>Jetrix 0.5 (1.0 RC)</h2>
+
+<ul>
+  <li>Game recording &amp; playback</li>
+  <li>Windows service</li>
+  <li>Unix Daemon</li>
+</ul>
 
 <? include("footer.inc.php") ?>
