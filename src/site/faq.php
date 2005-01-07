1e651745d674bdac9eedcff6fa26993a75483859
Emmanuel Bourg <smanux@lfjr.net>
2005-01-07 10:53:39 +0000

    Added "How do I log on the administration console?"

--- src/site/faq.php
+++ src/site/faq.php
@@ -2,6 +2,18 @@
 
 <h1>Frequently Asked Questions</h1>
 
-Send your questions to <a href="mailto:smanux@lfjr.net">smanux@lfjr.net</a> ! :)
+Send your questions to <a href="mailto:smanux@lfjr.net">smanux@lfjr.net</a> or
+post them on the <a href="http://sourceforge.net/forum/forum.php?forum_id=172941">forum</a>
+
+<h3>How do I log on the administration console?</h3>
+
+<p>Open your browser and go to your server on the 8080 port:</p>
+
+<code>http://tetrinet.yourhostname.net:8080</code>
+
+<p>A username and a password are requested, type <tt>admin</tt>, as username,
+and use the administrator password defined in the <tt>server.xml</tt> file. The
+default password is <tt>adminpass</tt>.</p>
+
 
 <? include("footer.inc.php") ?>
