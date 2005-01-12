f1464c0fa8a842acf40a4147c04fc58c5519c468
Emmanuel Bourg <smanux@lfjr.net>
2005-01-12 18:56:30 +0000

    Moved the patcher to the net.jetrix.tools.patcher package

--- src/java/net/jetrix/tools/patcher/JetrixUpdate.java
+++ src/java/net/jetrix/tools/patcher/JetrixUpdate.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-package net.jetrix.patcher;
+package net.jetrix.tools.patcher;
 
 import java.io.*;
 import java.net.*;
