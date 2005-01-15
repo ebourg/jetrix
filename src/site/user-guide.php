3704828da6c855f66975cab5414bb7a83feda3e0
Emmanuel Bourg <smanux@lfjr.net>
2005-01-15 21:05:18 +0000

    installation notes update listeners introduction

--- src/site/user-guide.php
+++ src/site/user-guide.php
@@ -9,10 +9,11 @@
   <li><a href="#section2">Configuration</a>
     <ul>
       <li><a href="#section2-1">Listeners</a></li>
-      <li><a href="#section2-2">Channels</a></li>
-      <li><a href="#section2-3">Commands</a></li>
-      <li><a href="#section2-4">Filters</a></li>
-      <li><a href="#section2-5">Banlist</a></li>
+      <li><a href="#section2-2">Services</a></li>
+      <li><a href="#section2-3">Channels</a></li>
+      <li><a href="#section2-4">Commands</a></li>
+      <li><a href="#section2-5">Filters</a></li>
+      <li><a href="#section2-6">Banlist</a></li>
     </ul>
   </li>
   <li><a href="#section3">Web Administration</a></li>
@@ -62,6 +63,7 @@
   <li>Java Runtime Environnement 1.5.x or higher</li>
   <li>32 Mb RAM</li>
   <li>5 Mb hard drive space</li>
+  <li>If you have a firewall, open the ports 31456, 31457 and 31458</li>
 </ul>
 
 <h3>Running &amp; Upgrading</h3>
@@ -71,27 +73,75 @@ download it here :</p>
 
 <a href="http://java.sun.com/j2se/1.5.0/download.jsp">http://java.sun.com/j2se/1.5.0/download.jsp</a>
 
-<p>Make sure the <tt>JAVA_HOME</tt> environnement variable is properly set !
-Unzip the Jetrix archive to the installation directory and run the following
-commands in the jetrix directory:</p>
+<h4>Unix</h4>
 
-<ul>
-  <li>To start the server, <tt>./jetrix</tt> on Unix or <tt>jetrix</tt> on Windows</li>
-  <li>To download the latest release and upgrade your version, <tt>./update</tt> on Unix or <tt>update</tt> on Windows</li>
-</ul>
+<p>Once the Java Runtime Environnement is installed, you'll need a JAVA_HOME
+environnement variable pointing to your Java directory. For example on
+Linux you can add this line to your /etc/profile file:</p>
+
+<code>export JAVA_HOME=/usr/java/j2sdk_1.4.2</code>
+
+<p>Then decompress the JetriX archive to the installation directory:</p>
+
+<code>tar -jxvf jetrix-x.y.z.tar.bz2</code>
+
+<p>To start the server, type the following commands in the jetrix directory:</p>
+
+<code>./jetrix</code>
+
+<p>To start the server as a background process, type:</p>
+
+<code>nohup ./jetrix &amp;</code>
+
+<p>Or even better if the screen tool is available on your system, type:</p>
+
+<code>screen ./jetrix</code>
+
+<p>To download the latest release and upgrade your version (experimental), type:</p>
+
+<code>./update</code>
+
+<h4>Windows</h4>
+
+<p>Install the Java Runtime Environnement and then run the Jetrix installer (<tt>jetrix-x.y.z-installer.exe</tt>).
+This will create a "Jetrix TetriNET Server" group in your Start menu, just click on "Jetrix TetriNET Server" to
+start Jetrix, an icon will appear in the system tray:</p>
+
+<center><img src="images/systray.png"/></center>
+
+<p>You can click on this icon to open a menu and stop the server or open the administration console.</p>
 
 
 <h1><a id="section2"></a>Configuration</h1>
 
 <h2><a id="section2-1"></a>Listeners</h2>
 
-<h2><a id="section2-2"></a>Channels</h2>
+A listener is a component listening for network connections. It waits for connections on a specific port, for example
+the listener waiting for tspec clients listens on the port 31458. The listeners are declared in the server configuration
+under the <tt>&lt;listeners&gt;</tt> element:
 
-<h2><a id="section2-3"></a>Commands</h2>
+<div class="code">
+  &lt;!-- Client listeners --&gt;
+  &lt;listeners>
+    &lt;listener class="net.jetrix.listeners.TetrinetListener"/&gt;
+    &lt;listener class="net.jetrix.listeners.TSpecListener"/&gt;
+    &lt;listener class="net.jetrix.listeners.IRCListener" port="31456"/&gt;
+    &lt;listener class="net.jetrix.listeners.HttpListener" port="8080"/&gt;
+  &lt;/listeners&gt;
 
-<h2><a id="section2-4"></a>Filters</h2>
+</div>
 
-<h2><a id="section2-5"></a>Banlist</h2>
+
+
+<h2><a id="section2-2"></a>Services</h2>
+
+<h2><a id="section2-3"></a>Channels</h2>
+
+<h2><a id="section2-4"></a>Commands</h2>
+
+<h2><a id="section2-5"></a>Filters</h2>
+
+<h2><a id="section2-6"></a>Banlist</h2>
 
 
 <h1><a id="section3"></a>Web Administration</h1>
