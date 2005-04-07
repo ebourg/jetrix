6725dd4793844536c2e7784a4775c2857f469524
Emmanuel Bourg <smanux@lfjr.net>
2005-04-07 23:19:24 +0000

    Added the WebSpec demonstration page

---
+++ src/site/webspec.php
@@ -0,0 +1,16 @@
+<? include("header.inc.php") ?>
+
+<h1>WebSpec</h1>
+
+<p>Welcome on the WebSpec demonstration page. WebSpec is a simplified TSpec
+client to watch the games in progress on a TetriNET server. It can be easily
+embedded in a web site and is a natural companion for any TetriNET server.
+WebSpec is based on the code of Jetrix and will be released under the GPL.</p>
+
+<p>In order to try WebSpec you have to install the <a href="http://java.sun.com/j2se/1.5.0/download.jsp">Java Runtime Environment 5.0</a></p>
+
+<div align="center">
+  <iframe src="http://tetrinet.fr/applet/webspec.html" width="640" height="480" frameborder="0" scrolling="no"></iframe>
+</div>
+
+<? include("footer.inc.php") ?>
