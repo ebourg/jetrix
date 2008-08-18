fd0ed15eaf86748143d93503fd5a07d1f09127dd
Emmanuel Bourg <smanux@lfjr.net>
2008-08-18 14:27:10 +0000

    Fixed QueryAgent on systems with UTF8 as default encoding Added a connect time out to QueryAgent

--- src/java/net/jetrix/agent/QueryAgent.java
+++ src/java/net/jetrix/agent/QueryAgent.java
@@ -19,10 +19,13 @@
 
 package net.jetrix.agent;
 
-import net.jetrix.Message;
-import net.jetrix.protocols.QueryProtocol;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,8 +33,13 @@ import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.jetrix.Message;
+import net.jetrix.protocols.QueryProtocol;
+
 /**
+ * Agent performing query requests on a TetriNET server.
  *
+ * @see <a href="See http://jetrix.sourceforge.net/dev-guide.php#section2-4">Query Protocol Documentation</a>
  *
  * @author Emmanuel Bourg
  * @version $Revision$, $Date$
@@ -48,9 +56,10 @@ public class QueryAgent implements Agent
     public void connect(String hostname) throws IOException
     {
         this.hostname = hostname;
-        socket = new Socket(hostname, 31457);
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        socket = new Socket();
+        socket.connect(new InetSocketAddress(hostname, 31457), 5000);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "ISO-8859-1"));
+        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "ISO-8859-1"));
         socket.setSoTimeout(10000);
     }
 
