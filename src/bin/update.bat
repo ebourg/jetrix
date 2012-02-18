1dd801d03b658b0ba932493bfc608a9c1026cf25
Emmanuel Bourg <smanux@lfjr.net>
2012-02-18 15:00:17 +0000

    Removed the update system

--- src/bin/update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-SETLOCAL
-
-IF NOT JAVA_HOME == "" SET JAVA_EXE="%JAVA_HOME%\bin\java"
-IF JAVA_HOME == "" SET JAVA_EXE=java
-
-%JAVA_EXE% -cp lib/jetrix-@version@.jar net.jetrix.tools.patcher.JetrixUpdate
